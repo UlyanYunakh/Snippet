9687a88868f0bf432963093b26a7a2e865bf8d9a
UlyanYunakh <ulyanyunakh@gmail.com>
2021-08-02 14:46:25 +0300

    Add lang, user and tags instance for testing.

--- SnippetProject/Extensions/SnippetPostHelper/SnippetPostExtensions.cs
+++ SnippetProject/Extensions/SnippetPostHelper/SnippetPostExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using Snippet.Services.Models;
 
 namespace SnippetProject.Extensions.SnippetPostHelper
@@ -12,6 +13,17 @@ namespace SnippetProject.Extensions.SnippetPostHelper
             post.Description = "default description";
             post.Snippet = "default snippet";
             post.Date = DateTime.Now;
+            post.Language = new Language() { Id = 1, Name = "Haskell" };
+            post.Tags = new List<Tag>(){
+                new Tag() { Id = 2, Name="#tag" },
+                new Tag() { Id = 2, Name="#tag" },
+                new Tag() { Id = 2, Name="#tag" },
+                new Tag() { Id = 2, Name="#tag" },
+                new Tag() { Id = 2, Name="#tag" },
+                new Tag() { Id = 2, Name="#tag" },
+                new Tag() { Id = 2, Name="#tag" }
+            };
+            post.User = new User() { Id = 3, Username = "Ulyan" };
             return post;
         }
 
@@ -21,6 +33,17 @@ namespace SnippetProject.Extensions.SnippetPostHelper
             post.Title = "default title";
             post.Description = "default description";
             post.Date = DateTime.Now;
+            post.Language = new Language() { Id = 1, Name = "Haskell" };
+            post.User = new User() { Id = 3, Username = "Ulyan" };
+            post.Tags = new List<Tag>(){
+                new Tag() { Id = 2, Name="#tag" },
+                new Tag() { Id = 2, Name="#tag" },
+                new Tag() { Id = 2, Name="#tag" },
+                new Tag() { Id = 2, Name="#tag" },
+                new Tag() { Id = 2, Name="#tag" },
+                new Tag() { Id = 2, Name="#tag" },
+                new Tag() { Id = 2, Name="#tag" }
+            };
             return post;
         }
     }
